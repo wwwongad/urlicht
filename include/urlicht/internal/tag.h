7565fcdac4085a26814b5e039bc9eb9c37f58adb
wwwongad <peterwong2020a@gmail.com>
2026-04-24 17:21:20 +0800

    Add tags for sorted and sorted-unique inputs

--- include/urlicht/internal/tag.h
+++ include/urlicht/internal/tag.h
@@ -6,10 +6,10 @@
 namespace urlicht {
 
     namespace detail {
-        template <typename T>
+        template <typename>
         struct inplace_t {};
 
-        template <typename T>
+        template <typename>
         struct inplace_cond_t {};
 
         template <auto V>
@@ -19,23 +19,47 @@ namespace urlicht {
             }
         };
 
-        template <typename T>
+        struct sorted_t {};
+
+        struct sorted_unique_t {};
+
+        struct heapified_t {};
+
+        template <typename>
         struct is_urlicht_inplace_t : std::false_type {};
 
         template <typename T>
         struct is_urlicht_inplace_t<inplace_t<T>> : std::true_type {};
 
-        template <typename T>
+        template <typename>
         struct is_urlicht_inplace_cond_t : std::false_type {};
 
         template <typename T>
         struct is_urlicht_inplace_cond_t<inplace_cond_t<T>> : std::true_type {};
 
-        template <typename T>
+        template <typename>
         struct is_urlicht_nontype_t : std::false_type {};
 
         template <auto V>
         struct is_urlicht_nontype_t<nontype_t<V>> : std::true_type {};
+
+        template <typename>
+        struct is_urlicht_sorted_t : std::false_type {};
+
+        template <>
+        struct is_urlicht_sorted_t<sorted_t> : std::true_type {};
+
+        template <typename>
+        struct is_urlicht_sorted_unique_t : std::false_type {};
+
+        template <>
+        struct is_urlicht_sorted_unique_t<sorted_unique_t> : std::true_type {};
+
+        template <typename>
+        struct is_urlicht_heapified_t : std::false_type {};
+
+        template <>
+        struct is_urlicht_heapified_t<heapified_t> : std::true_type {};
     }
 
     template <typename T>
@@ -55,6 +79,21 @@ namespace urlicht {
 
     template <typename T>
     inline constexpr bool is_urlicht_nontype_v = detail::is_urlicht_nontype_t<T>::value;
+
+    inline constexpr detail::sorted_t sorted{};
+
+    template <typename T>
+    inline constexpr bool is_urlicht_sorted_v = detail::is_urlicht_sorted_t<T>::value;
+
+    inline constexpr detail::sorted_unique_t sorted_unique{};
+
+    template <typename T>
+    inline constexpr bool is_urlicht_sorted_unique_v = detail::is_urlicht_sorted_unique_t<T>::value;
+
+    inline constexpr detail::heapified_t heapified{};
+
+    template <typename T>
+    inline constexpr bool is_urlicht_heapified_v = detail::is_urlicht_heapified_t<T>::value;
 
 }
 #endif //URLICHT_TAG_H
