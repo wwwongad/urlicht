fe689727c20b92108309b6c2df7180f0ceaa4737
wwwongad <peterwong2020a@gmail.com>
2026-06-10 22:37:17 +0800

    Refactor concurrent_arena class structure
    Refactor concurrent_arena to inherit from arena and streamline methods.

--- include/urlicht/memory/concurrent_arena.h
+++ include/urlicht/memory/concurrent_arena.h
@@ -5,7 +5,6 @@
 #include <urlicht/memory/arena.h>
 #include <mutex>
 #include <concepts>
-#include <type_traits>
 #include <cstddef>
 
 namespace urlicht {
@@ -15,101 +14,56 @@ namespace urlicht {
      *        It preserves all methods provided by urlicht::arena. See arena.h for detail.
      */
     template <bool UseUpstream,
-              ArenaGrowthPolicy GrowthPolicy,
+              arena_growth_policy GrowthPolicy,
               concepts::allocator UpstreamAlloc>
-    class concurrent_arena {
+    class concurrent_arena : private arena<UseUpstream, GrowthPolicy, UpstreamAlloc> {
         static_assert(std::same_as<typename UpstreamAlloc::value_type, std::byte>,
             "Upstream allocator must allocate std::byte");
+
     public:
         using upstream_allocator = UpstreamAlloc;
         using upstream_traits = std::allocator_traits<upstream_allocator>;
         using arena_type = arena<UseUpstream, GrowthPolicy, UpstreamAlloc>;
+        using arena_type::arena_type;
+        using allocation_result = typename arena_type::allocation_result;
 
-        static constexpr size_t default_align = alignof(std::max_align_t);
-    private:
-        std::mutex mutex_;
-        arena_type arena_;
-    public:
-        static consteval bool use_upstream() noexcept {
-            return UseUpstream;
-        }
+        static constexpr std::size_t default_align = alignof(std::max_align_t);
 
-        static consteval ArenaGrowthPolicy growth_policy() noexcept requires(UseUpstream) {
-            return GrowthPolicy;
-        }
+        concurrent_arena(const concurrent_arena&) = delete;
+        concurrent_arena& operator=(const concurrent_arena&) = delete;
+        concurrent_arena(concurrent_arena&&) = delete;
+        concurrent_arena& operator=(concurrent_arena&&) = delete;
 
-        /************************* CONSTRUCTORS ****************************/
-        /**
-         * @brief Constructor delegating entirely to the constructors of urlicht::arena.
-         */
-        template <typename... Args>
-        requires std::constructible_from<arena_type, Args&&...>
-             && (!is_urlicht_arena_v<std::remove_cvref_t<Args>> && ...)
-             && (!is_urlicht_concurrent_arena_v<std::remove_cvref_t<Args>> && ...)
-        constexpr concurrent_arena(Args&& ...args)
-        noexcept(std::is_nothrow_constructible_v<arena_type, Args&&...>)
-        : arena_{std::forward<Args>(args)...} { }
-
-        constexpr concurrent_arena(const arena_type&) = delete;
-        constexpr concurrent_arena& operator=(const arena_type&) = delete;
-
-        constexpr concurrent_arena(arena_type&& arena)
-        noexcept(std::is_nothrow_move_constructible_v<arena_type>)
-        : arena_{std::move(arena)} { }
-
-        constexpr concurrent_arena& operator=(arena_type&& arena)
-        noexcept(std::is_nothrow_move_assignable_v<arena_type>) {
-            if (&arena_ != &arena) {
-                arena_ = std::move(arena);
-            }
-            return *this;
-        }
-
-        constexpr concurrent_arena(const concurrent_arena&) = delete;
-        constexpr concurrent_arena& operator=(const concurrent_arena&) = delete;
-
-        constexpr concurrent_arena(concurrent_arena&& concurrent_arena)
-        noexcept(std::is_nothrow_move_constructible_v<arena_type>)
-        : arena_{std::move(concurrent_arena.arena_)} { }
-
-        constexpr concurrent_arena& operator=(concurrent_arena&& other)
-        noexcept(std::is_nothrow_move_assignable_v<arena_type>) {
-            if (this != &other) {
-                arena_ = std::move(other.arena_);
-            }
-            return *this;
-        }
-
-        constexpr ~concurrent_arena() noexcept = default;
+        constexpr ~concurrent_arena() = default;
 
         /**
          * @brief Returns a const reference to the underlying arena.
          */
         [[nodiscard]] constexpr const arena_type& get_arena() const noexcept {
-            return arena_;
+            return static_cast<arena_type&>(*this);
         }
 
-        /************************* CORE METHODS ****************************/
+        //************************* CORE METHODS ****************************//
 
         UL_CONSTEXPR23 void release() noexcept {
             std::unique_lock lock(mutex_);
-            arena_.release();
+            get_arena_mut_().release();
         }
 
         UL_CONSTEXPR23 void reset() noexcept {
             std::unique_lock lock(mutex_);
-            arena_.reset();
+            get_arena_mut_().reset();
         }
 
         [[nodiscard]] UL_CONSTEXPR23 void* unchecked_allocate_initial(const size_t bytes,
                                                                       const size_t align = default_align) noexcept {
             std::unique_lock lock(mutex_);
-            return arena_.unchecked_allocate_initial(bytes, align);
+            return get_arena_mut_().unchecked_allocate_initial(bytes, align).ptr;
         }
 
-        [[nodiscard]] UL_CONSTEXPR23 void* allocate(const size_t bytes, const size_t align = default_align) {
+        [[nodiscard]] UL_CONSTEXPR23 auto allocate(const size_t bytes, const size_t align = default_align) {
             std::unique_lock lock(mutex_);
-            return arena_.allocate(bytes, align);
+            return get_arena_mut_().allocate(bytes, align);
         }
 
         static constexpr void deallocate(void*, size_t, [[maybe_unused]] size_t align = default_align) noexcept
@@ -118,6 +72,12 @@ namespace urlicht {
         friend constexpr bool operator==(const concurrent_arena& lhs, const concurrent_arena& rhs) noexcept {
             return std::addressof(lhs) == std::addressof(rhs);
         }
+    private:
+        [[nodiscard]] constexpr arena_type& get_arena_mut_() noexcept {
+            return static_cast<arena_type&>(*this);
+        }
+        // Data member
+        std::mutex mutex_;
     };
 }
 
